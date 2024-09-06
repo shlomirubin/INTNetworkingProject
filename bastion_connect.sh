bd4559f0ec9ae83fcff32d5087f7f3168c40922d
srubin <sru0815@gmail.com>
2024-09-06 11:24:07 +0300

    he

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -14,7 +14,7 @@ fi
 
 PUBLIC_IP=$1
 
-# Case 2: Connect to the public instance
+# Connect to the public instance
 if [ $# -eq 1 ]; then
   ssh -i "$KEY_PATH" ubuntu@"$PUBLIC_IP"
   exit $?
@@ -22,14 +22,15 @@ fi
 
 PRIVATE_IP=$2
 
-# Case 1 or Case 3: Connect to the private instance via the public instance
+#Connect to the private instance via the public instance
 if [ $# -ge 2 ]; then
   if [ $# -eq 2 ]; then
-    ssh -i "$KEY_PATH" -J ubuntu@"$PUBLIC_IP" ubuntu@"$PRIVATE_IP"
+    ssh -t -i "$KEY_PATH" ubuntu@"$PUBLIC_IP" "ssh -t -i /home/ubuntu/.ssh/SRubinKeyPrivate.pem" ubuntu@"$PRIVATE_IP"
   else
     # Case 3: Run a command on the private instance
     shift 2
-    ssh -i "$KEY_PATH" -J ubuntu@"$PUBLIC_IP" ubuntu@"$PRIVATE_IP" "$@"
+    ssh -i "$KEY_PATH" ubuntu@"$PUBLIC_IP" "ssh -i /home/ubuntu/.ssh/SRubinKeyPrivate.pem" ubuntu@"$PRIVATE_IP" "$@"
   fi
   exit $?
 fi
+
