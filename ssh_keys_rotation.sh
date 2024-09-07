91fd64ae1dfaf6393142080be1a191eb616ac350
srubin <sru0815@gmail.com>
2024-09-07 20:46:51 +0300

    key rotation and EC2 updated private IP again

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -13,16 +13,18 @@ NEW_KEY_NAME="RotatedKey"
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/$NEW_KEY_NAME -q -N ""
 
 # Step 2: Copy the new public key to the private instance
-ssh-copy-id -i ~/.ssh/${NEW_KEY_NAME}.pub ubuntu@"$PRIVATE_IP"
+ssh-copy-id -i ~/.ssh/${NEW_KEY_NAME}.pub -o "IdentitiesOnly=yes" -i $KEY_PATH ubuntu@"$PRIVATE_IP"
 
-# Step 3: Remove the old public key from the private instance
-OLD_KEY=$(cat $KEY_PATH.pub)  # Use the key defined in the test script
-ssh ubuntu@"$PRIVATE_IP" "sed -i '/$OLD_KEY/d' ~/.ssh/authorized_keys"
+# Step 3: Retrieve the old public key from the private instance
+OLD_KEY=$(ssh -i $KEY_PATH ubuntu@"$PRIVATE_IP" "cat ~/.ssh/authorized_keys")
 
-# Step 4: Test connection with the new key
+# Step 4: Remove the old public key from the private instance
+ssh ubuntu@"$PRIVATE_IP" "grep -v '$OLD_KEY' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys"
+
+# Step 5: Test connection with the new key
 ssh -i ~/.ssh/${NEW_KEY_NAME} ubuntu@"$PRIVATE_IP" "echo 'Successfully connected with the new key!'"
 
-# Step 5: Test connection with the old key (should fail)
+# Step 6: Test connection with the old key (should fail)
 if ssh -i $KEY_PATH ubuntu@"$PRIVATE_IP"; then
   exit 1  # Exit with error if the old key still works
 fi
