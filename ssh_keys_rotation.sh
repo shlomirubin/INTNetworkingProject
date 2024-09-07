57947040f36d20b8153463d691e22ea2dfa24b98
srubin <sru0815@gmail.com>
2024-09-07 21:22:08 +0300

    key rotation and EC2 updated private IP again

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -9,16 +9,19 @@ fi
 PRIVATE_IP=$1
 NEW_KEY_NAME="RotatedKey"
 KEY_PATH=$HOME/.ssh/SRubinKeyPrivate.pem
+PUB_KEY_PATH=~/.ssh/${NEW_KEY_NAME}.pub
 
 # Step 1: Create a new pair of keys
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/$NEW_KEY_NAME -q -N ""
 
-# Step 2: Copy the new public key to the private instance
-ssh-copy-id -i ~/.ssh/${NEW_KEY_NAME}.pub -o "IdentitiesOnly=yes" -i $KEY_PATH ubuntu@"$PRIVATE_IP"
+# Step 2: Copy the public key to the private instance
+scp $PUB_KEY_PATH ubuntu@"$PRIVATE_IP":/home/ubuntu/
 
-#Retrieve the old public key from the private instance
+# Append the new public key to the authorized_keys file
+ssh -i $KEY_PATH ubuntu@"$PRIVATE_IP" "cat /home/ubuntu/$(basename $PUB_KEY_PATH) >> ~/.ssh/authorized_keys && rm /home/ubuntu/$(basename $PUB_KEY_PATH)"
+
+# Retrieve the old public key from the private instance
 OLD_KEY=$(ssh -i $KEY_PATH ubuntu@"$PRIVATE_IP" "cat ~/.ssh/authorized_keys")
 
-#Remove the old public key from the private instance
-ssh ubuntu@"$PRIVATE_IP" "grep -v '$OLD_KEY' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys"
-
+# Remove the old public key from the private instance
+ssh -i $KEY_PATH ubuntu@"$PRIVATE_IP" "grep -v '$OLD_KEY' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys"
