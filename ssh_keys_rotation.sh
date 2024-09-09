6381f272d8e9107b59bcea213e132693c1e26799
srubin <sru0815@gmail.com>
2024-09-09 18:42:57 +0300

    key rotation and EC2 updated private IP againnnnnnnnn

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -11,7 +11,6 @@ NEW_KEY_NAME="RotatedKey"
 KEY_PATH2=$HOME/.ssh/SRubinKeyPrivate.pem
 PUB_KEY_PATH=~/.ssh/${NEW_KEY_NAME}.pub
 
-
 # Step 1: Create a new pair of keys
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/$NEW_KEY_NAME -q -N ""
 
@@ -21,6 +20,7 @@ scp -i "$KEY_PATH2" "$PUB_KEY_PATH" ubuntu@"$PRIVATE_IP":/home/ubuntu/
 
 # Append the new public key to the authorized_keys file
 ssh -i $KEY_PATH2 ubuntu@"$PRIVATE_IP" "cat /home/ubuntu/$(basename $PUB_KEY_PATH) >> ~/.ssh/authorized_keys && rm /home/ubuntu/$(basename $PUB_KEY_PATH)"
+
 
 # Retrieve the old public key from the private instance
 OLD_KEY=$(ssh -i $KEY_PATH2 ubuntu@"$PRIVATE_IP" "cat ~/.ssh/authorized_keys")
