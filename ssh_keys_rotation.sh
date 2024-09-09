bbf2edd64bf0e4831ebddb32969df248a8cd0047
srubin <sru0815@gmail.com>
2024-09-09 20:03:14 +0300

    Maybe again

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -17,14 +17,5 @@ ssh-keygen -t rsa -b 2048 -f ~/.ssh/$NEW_KEY_NAME -q -N ""
 # Step 2: Copy the public key to the private instance
 scp -i "$KEY_PATH2" "$PUB_KEY_PATH" ubuntu@"$PRIVATE_IP":/home/ubuntu/
 
-
 # Append the new public key to the authorized_keys file
-ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "cat /home/ubuntu/$(basename $PUB_KEY_PATH) >> ~/.ssh/authorized_keys && rm /home/ubuntu/$(basename $PUB_KEY_PATH)"
-
-
-# Retrieve the old public key from the private instance
-OLD_KEY=$(ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "cat ~/.ssh/authorized_keys")
-
-# Remove the old public key from the private instance
-ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "grep -v '$OLD_KEY' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys"
-
+ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "cat /home/ubuntu/$(basename $PUB_KEY_PATH) > ~/.ssh/authorized_keys && rm /home/ubuntu/$(basename $PUB_KEY_PATH)"
