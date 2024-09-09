cc972128f5746e93b998656db84a603cca1f681e
srubin <sru0815@gmail.com>
2024-09-09 21:30:23 +0300

    please make it work for me

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -16,5 +16,5 @@ scp -i "$KEY_PATH2" "$PUB_KEY_PATH" ubuntu@"$PRIVATE_IP":/home/ubuntu/
 # Append the new public key to the authorized_keys file
 ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "cat /home/ubuntu/RotatedKey.pub >> ~/.ssh/authorized_keys && rm /home/ubuntu/RotatedKey.pub"
 
-
+ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "grep -v 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCBYg6dkTTYhaUxhnwNSjyIJit6ExdhF1ZdSkajR7GbCCARZ+J3l37l5iElN/XOSWu6+1dnQoTSMDWvAR8eXhs727NLYj35BD3euAfWnrLTl93TQSEnbl04G9EjKZJ25C4VeI2hO3g/63yO4INKOWjVaT2SzSFFdgj39WmoiEqPZpJ53ComHkFJ+SWhwttNEebBCf/mTOBNVQnLBo9ur0ng729DnWg6fDvu4pz/cDF31gF8FFE/NE8KT47a8FiQ5jUBC3aVa4bIbi85ZRpT/sEuQ1JRoO81IF8E9Tz46bdJCP3Zsim072uSF/APt93DRiy7ZxdJowHeTHUqYimlKf8Z SRubinKeys'  ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys""
 
