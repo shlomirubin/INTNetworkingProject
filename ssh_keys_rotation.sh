0c749bf17df244a2c5ed1604d6e40467c680d783
srubin <sru0815@gmail.com>
2024-09-09 19:24:56 +0300

    Maybe?

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -9,7 +9,7 @@ fi
 PRIVATE_IP=$1
 NEW_KEY_NAME="RotatedKey"
 KEY_PATH2=$HOME/.ssh/SRubinKeyPrivate.pem
-PUB_KEY_PATH=~/.ssh/${NEW_KEY_NAME}.pub
+PUB_KEY_PATH=~/.ssh/$NEW_KEY_NAME.pub
 
 # Step 1: Create a new pair of keys
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/$NEW_KEY_NAME -q -N ""
@@ -19,12 +19,12 @@ scp -i "$KEY_PATH2" "$PUB_KEY_PATH" ubuntu@"$PRIVATE_IP":/home/ubuntu/
 
 
 # Append the new public key to the authorized_keys file
-ssh -i $KEY_PATH2 ubuntu@"$PRIVATE_IP" "cat /home/ubuntu/$(basename $PUB_KEY_PATH) >> ~/.ssh/authorized_keys && rm /home/ubuntu/$(basename $PUB_KEY_PATH)"
+ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "cat /home/ubuntu/$(basename $PUB_KEY_PATH) >> ~/.ssh/authorized_keys && rm /home/ubuntu/$(basename $PUB_KEY_PATH)"
 
 
 # Retrieve the old public key from the private instance
-OLD_KEY=$(ssh -i $KEY_PATH2 ubuntu@"$PRIVATE_IP" "cat ~/.ssh/authorized_keys")
+OLD_KEY=$(ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "cat ~/.ssh/authorized_keys")
 
 # Remove the old public key from the private instance
-ssh -i $KEY_PATH2 ubuntu@"$PRIVATE_IP" "grep -v '$OLD_KEY' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys"
+ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "grep -v '$OLD_KEY' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys"
 
