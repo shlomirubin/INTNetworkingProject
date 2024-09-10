c469336c896980cb5bc262d0453d51b0417e5960
srubin <sru0815@gmail.com>
2024-09-10 19:57:27 +0300

    with alon again

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -9,6 +9,7 @@ PRIVATE_IP=$1
 NEW_KEY_NAME="RotatedKey"
 KEY_PATH2=$HOME/.ssh/SRubinKeyPrivate.pem
 PUB_KEY_PATH=~/.ssh/$NEW_KEY_NAME.pub
+NEW_PRIVATE_KEY=~/.ssh/RotationKey
 
 # Step 1: Create a new pair of keys
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/$NEW_KEY_NAME -q -N ""
@@ -17,6 +18,6 @@ scp -i "$KEY_PATH2" "$PUB_KEY_PATH" ubuntu@"$PRIVATE_IP":/home/ubuntu/
 # Append the new public key to the authorized_keys file
 ssh -i "$KEY_PATH2" ubuntu@"$PRIVATE_IP" "cat /home/ubuntu/RotatedKey.pub > ~/.ssh/authorized_keys && rm /home/ubuntu/RotatedKey.pub"
 
-mv "$NEW_KEY_NAME" "$KEY_PATH2"
+mv "$NEW_PRIVATE_KEY" "$KEY_PATH2"
 
 
